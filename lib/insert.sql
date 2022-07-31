031ef3ead5b3deb4588553151210f69a097ca0b5
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-07-31 09:23:53 -0400

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 8, 'M', 'brown', 'lazy', 1),
+('Rowdy', 3, 'M', 'black', 'happy', 0),
+('Tabitha', 8, 'F', 'pink', 'friendly', 1),
+('Sergeant Brown', 5, 'M', 'black', 'happy', 1),
+('Melissa', 9, 'F', 'brown', 'moody', 1),
+('Grinch', 3, 'M', 'green', 'moody', 1),
+('Wendy', 2, 'F', 'pink', 'happy', 1),
+(NULL, 15, 'M', 'black', 'friendly', 0);
